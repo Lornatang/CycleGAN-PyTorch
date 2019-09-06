340a9aa0ae070fe7fa6db90a7949a8e9c2c00430
Changyu_Liu <shiyipaisizuo@163.com>
2019-09-06 12:12:32 +0800

    Create datasets.py

---
+++ datasets.py
@@ -0,0 +1,44 @@
+# Copyright 2019 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+import glob
+import os
+import random
+
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class ImageDataset(Dataset):
+  def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
+    self.transform = transforms.Compose(transforms_)
+    self.unaligned = unaligned
+
+    self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
+    self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
+
+  def __getitem__(self, index):
+    item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+
+    if self.unaligned:
+      item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+    else:
+      item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+
+    return {'A': item_A, 'B': item_B}
+
+  def __len__(self):
+    return max(len(self.files_A), len(self.files_B))
