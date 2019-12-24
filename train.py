d1a82aafb433811e87cdb7369b842b48fc33f5c6
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-24 23:12:01 +0800

    commit init

--- train.py
+++ train.py
@@ -1,206 +1,78 @@
-# Copyright 2019 Lorna Authors. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+"""General-purpose training script for image-to-image translation.
 
-"""This command will start a training session using the images under the
-   dataroot/train directory with the hyperparameters that showed best results
-   according to CycleGAN authors. You are free to change those hyperparameters,
-   see python3 train.py --help for a description of those."""
+This script works for various models (with option '--model': e.g., pix2pix, cyclegan, colorization) and
+different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
+You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
 
-import argparse
+It first creates model, dataset, and visualizer given the option.
+It then does standard network training. During the training, it also visualize/save the images, print/save the loss plot, and save models.
+The script supports continue/resume training. Use '--continue_train' to resume your previous training.
 
-import itertools
-import torch
-import torchvision.transforms as transforms
-from PIL import Image
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
+Example:
+    Train a CycleGAN model:
+        python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
+    Train a pix2pix model:
+        python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
 
-from datasets import ImageDataset
-from models import Discriminator
-from models import Generator
-from utils import LambdaLR
-from utils import ReplayBuffer
-from utils import weights_init_normal
+See options/base_options.py and options/train_options.py for more training options.
+See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
+See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
+"""
+import time
+from options.train_options import TrainOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import Visualizer
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
-parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
-parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
-parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
-parser.add_argument('--decay_epoch', type=int, default=100,
-                    help='epoch to start linearly decaying the learning rate to 0')
-parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
-parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
-parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
-parser.add_argument('--cuda', action='store_true', help='use GPU computation')
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-opt = parser.parse_args()
-print(opt)
+if __name__ == '__main__':
+    opt = TrainOptions().parse()   # get training options
+    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    dataset_size = len(dataset)    # get the number of images in the dataset.
+    print('The number of training images = %d' % dataset_size)
 
-if torch.cuda.is_available() and not opt.cuda:
-  print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    model = create_model(opt)      # create a model given opt.model and other options
+    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
+    total_iters = 0                # the total number of training iterations
 
-device = torch.device("cuda:0" if opt.cuda else "cpu")
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+        epoch_start_time = time.time()  # timer for entire epoch
+        iter_data_time = time.time()    # timer for data loading per iteration
+        epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
+        visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
 
-###### Definition of variables ######
-# Networks
-if torch.cuda.device_count() > 1:
-  netG_A2B = torch.nn.DataParallel(Generator(opt.input_nc, opt.output_nc))
+        for i, data in enumerate(dataset):  # inner loop within one epoch
+            iter_start_time = time.time()  # timer for computation per iteration
+            if total_iters % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
 
-  netG_B2A = torch.nn.DataParallel(Generator(opt.output_nc, opt.input_nc))
-  netD_A = torch.nn.DataParallel(Discriminator(opt.input_nc))
-  netD_B = torch.nn.DataParallel(Discriminator(opt.output_nc))
-else:
-  netG_A2B = Generator(opt.input_nc, opt.output_nc)
-  netG_B2A = Generator(opt.output_nc, opt.input_nc)
-  netD_A = Discriminator(opt.input_nc)
-  netD_B = Discriminator(opt.output_nc)
+            total_iters += opt.batch_size
+            epoch_iter += opt.batch_size
+            model.set_input(data)         # unpack data from dataset and apply preprocessing
+            model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
-netG_A2B.to(device)
-netG_B2A.to(device)
-netD_A.to(device)
-netD_B.to(device)
+            if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
+                save_result = total_iters % opt.update_html_freq == 0
+                model.compute_visuals()
+                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
-netG_A2B.apply(weights_init_normal)
-netG_B2A.apply(weights_init_normal)
-netD_A.apply(weights_init_normal)
-netD_B.apply(weights_init_normal)
+            if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
+                losses = model.get_current_losses()
+                t_comp = (time.time() - iter_start_time) / opt.batch_size
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                if opt.display_id > 0:
+                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
-# Lossess
-criterion_GAN = torch.nn.MSELoss()
-criterion_cycle = torch.nn.L1Loss()
-criterion_identity = torch.nn.L1Loss()
+            if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
+                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+                model.save_networks(save_suffix)
 
-# Optimizers & LR schedulers
-optimizer_G = torch.optim.Adam(itertools.chain(netG_A2B.parameters(), netG_B2A.parameters()),
-                               lr=opt.lr, betas=(0.5, 0.999))
-optimizer_D_A = torch.optim.Adam(netD_A.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-optimizer_D_B = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+            iter_data_time = time.time()
+        if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
+            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
+            model.save_networks('latest')
+            model.save_networks(epoch)
 
-lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G,
-                                                   lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A,
-                                                     lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B,
-                                                     lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-
-# Inputs & targets memory allocation
-Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
-input_A = Tensor(opt.batchSize, opt.input_nc, opt.size, opt.size)
-input_B = Tensor(opt.batchSize, opt.output_nc, opt.size, opt.size)
-target_real = Variable(Tensor(opt.batchSize).fill_(1.0), requires_grad=False)
-target_fake = Variable(Tensor(opt.batchSize).fill_(0.0), requires_grad=False)
-
-fake_A_buffer = ReplayBuffer()
-fake_B_buffer = ReplayBuffer()
-
-# Dataset loader
-transforms_ = [transforms.Resize(int(opt.size * 1.12), Image.BICUBIC),
-               transforms.RandomCrop(opt.size),
-               transforms.RandomHorizontalFlip(),
-               transforms.ToTensor(),
-               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-dataloader = DataLoader(ImageDataset(opt.dataroot, transforms_=transforms_, unaligned=True),
-                        batch_size=opt.batchSize, shuffle=True, num_workers=opt.n_cpu)
-
-for epoch in range(opt.epoch, opt.n_epochs):
-  for i, batch in enumerate(dataloader):
-    # Set model input
-    real_A = Variable(input_A.copy_(batch['A']))
-    real_B = Variable(input_B.copy_(batch['B']))
-
-    ###### Generators A2B and B2A ######
-    optimizer_G.zero_grad()
-
-    # Identity loss
-    # G_A2B(B) should equal B if real B is fed
-    same_B = netG_A2B(real_B)
-    loss_identity_B = criterion_identity(same_B, real_B) * 5.0
-    # G_B2A(A) should equal A if real A is fed
-    same_A = netG_B2A(real_A)
-    loss_identity_A = criterion_identity(same_A, real_A) * 5.0
-
-    # GAN loss
-    fake_B = netG_A2B(real_A)
-    pred_fake = netD_B(fake_B)
-    loss_GAN_A2B = criterion_GAN(pred_fake, target_real)
-
-    fake_A = netG_B2A(real_B)
-    pred_fake = netD_A(fake_A)
-    loss_GAN_B2A = criterion_GAN(pred_fake, target_real)
-
-    # Cycle loss
-    recovered_A = netG_B2A(fake_B)
-    loss_cycle_ABA = criterion_cycle(recovered_A, real_A) * 10.0
-
-    recovered_B = netG_A2B(fake_A)
-    loss_cycle_BAB = criterion_cycle(recovered_B, real_B) * 10.0
-
-    # Total loss
-    loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
-    loss_G.backward()
-
-    optimizer_G.step()
-    ###################################
-
-    ###### Discriminator A ######
-    optimizer_D_A.zero_grad()
-
-    # Real loss
-    pred_real = netD_A(real_A)
-    loss_D_real = criterion_GAN(pred_real, target_real)
-
-    # Fake loss
-    fake_A = fake_A_buffer.push_and_pop(fake_A)
-    pred_fake = netD_A(fake_A.detach())
-    loss_D_fake = criterion_GAN(pred_fake, target_fake)
-
-    # Total loss
-    loss_D_A = (loss_D_real + loss_D_fake) * 0.5
-    loss_D_A.backward()
-
-    optimizer_D_A.step()
-    ###################################
-
-    ###### Discriminator B ######
-    optimizer_D_B.zero_grad()
-
-    # Real loss
-    pred_real = netD_B(real_B)
-    loss_D_real = criterion_GAN(pred_real, target_real)
-
-    # Fake loss
-    fake_B = fake_B_buffer.push_and_pop(fake_B)
-    pred_fake = netD_B(fake_B.detach())
-    loss_D_fake = criterion_GAN(pred_fake, target_fake)
-
-    # Total loss
-    loss_D_B = (loss_D_real + loss_D_fake) * 0.5
-    loss_D_B.backward()
-
-    optimizer_D_B.step()
-    ###################################
-
-  # Update learning rates
-  lr_scheduler_G.step()
-  lr_scheduler_D_A.step()
-  lr_scheduler_D_B.step()
-
-  # Save models checkpoints
-  torch.save(netG_A2B.state_dict(), 'output/netG_A2B.pth')
-  torch.save(netG_B2A.state_dict(), 'output/netG_B2A.pth')
-  torch.save(netD_A.state_dict(), 'output/netD_A.pth')
-  torch.save(netD_B.state_dict(), 'output/netD_B.pth')
-###################################
+        print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+        model.update_learning_rate()                     # update learning rates at the end of every epoch.
