581b1559d579617c86da0fc364d761e196772f7c
liuchangyu <liuchangyu1111@gmail.com>
2020-05-11 12:00:34 +0800

    Add video file output mode

--- cyclegan_pytorch/datasets.py
+++ cyclegan_pytorch/datasets.py
@@ -14,8 +14,12 @@
 import glob
 import os
 import random
+import time
+import warnings
+from threading import Thread
 
 import cv2
+import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 
@@ -42,25 +46,78 @@ class ImageDataset(Dataset):
         return max(len(self.files_A), len(self.files_B))
 
 
-class VideoDataset(Dataset):
-    def __init__(self, root, transform=None, style="A"):
-        self.transform = transform
+class VideoDataset:
+    """ For reading camera or network data
 
-        capture = cv2.VideoCapture(root)
-        success, image = capture.read()
-        count = 0
-        while success:
-            if count % 1 == 0:
-                cv2.imwrite(os.path.join("data", root, f"{style}", f"{count}.jpg"), image)
-            success, image = capture.read()
-            count += 1
+    Load data types from data flow.
 
-        self.files = sorted(glob.glob(os.path.join("data", root, f"{style}") + "/*.*"))
+    Args:
+        dataroot (str): Data flow file name.
+        image_size (int): Image size in default data flow. (default:``416``).
+    """
 
-    def __getitem__(self, index):
-        items = self.transform(Image.open(self.files[index % len(self.files)]))
+    def __init__(self, dataroot, image_size=416):
 
-        return items
+        self.mode = "images"
+        self.image_size = image_size
+
+        sources = [dataroot]
+
+        n = len(sources)
+        self.images = [None] * n
+        self.sources = sources
+        for i, s in enumerate(sources):
+            # Start the thread to read frames from the video stream
+            print(f"{i + 1}/{n}: {s}... ", end="")
+
+            capture = cv2.VideoCapture(0 if s == "0" else s)
+            assert capture.isOpened(), f"Failed to open {s}"
+
+            width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = capture.get(cv2.CAP_PROP_FPS) % 100
+            _, self.images[i] = capture.read()  # guarantee first frame
+            thread = Thread(target=self.update, args=([i, capture]), daemon=True)
+            print(f"Success ({width}*{height} at {fps:.2f}FPS).")
+            thread.start()
+        print("")
+
+    def update(self, index, capture):
+        # Read next stream frame in a daemon thread
+        num = 0
+        while capture.isOpened():
+            num += 1
+            # Grabs the next frame from video file or capturing device.
+            capture.grab()
+            # read every 4th frame
+            if num == 4:
+                _, self.images[index] = capture.retrieve()
+                num = 0
+            time.sleep(0.01)  # wait time
+
+    def __iter__(self):
+        self.count = -1
+        return self
+
+    def __next__(self):
+        self.count += 1
+        raw_image = self.images.copy()
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            raise StopIteration
+
+        # Letterbox
+        image = [x for x in raw_image]
+
+        # Stack
+        image = np.stack(image, 0)
+
+        # BGR convert to RGB (batch_size 3 x 416 x 416)
+        image = image[:, :, :, ::-1].transpose(0, 3, 1, 2)  # BGR to RGB, to bsx3x416x416
+        # Return a contiguous array
+        image = np.ascontiguousarray(image)
+
+        return image, raw_image
 
     def __len__(self):
-        return len(self.files)
+        return 0
