340ed938dc94c4d7bf5d0e9eee68bdcec6824246
liuchangyu <liuchangyu1111@gmail.com>
2020-05-22 08:10:29 +0800

    Eliminate .data access for parameters as much as possible

--- cyclegan_pytorch/utils.py
+++ cyclegan_pytorch/utils.py
@@ -39,10 +39,11 @@ class ReplayBuffer:
         return torch.cat(to_return)
 
 
+# custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant(m.bias.data, 0.0)
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        torch.nn.init.normal_(m.weight, 1.0, 0.02)
+        torch.nn.init.zeros_(m.bias)
